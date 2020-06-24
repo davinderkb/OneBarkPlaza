539ab886a34aa9c2592eac379543fee3ecf9843d
Davinder Kumar <dkb37023@gmail.com>
2020-06-24 21:19:18 +0530

    Revert "ios changes"
    This reverts commit 27785a2f3f5a759fc19d446560c395c259c9d275.

--- android/app/src/profile/gen/bonafide/one_bark_plaza/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package bonafide.one_bark_plaza;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}
